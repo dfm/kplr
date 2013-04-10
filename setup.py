e920c60c1c687bbf4795b65a1af1993b4d6c30e9
Dan F-M <danfm@nyu.edu>
2013-04-10 14:40:51 -0400

    packaging

---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import (division, print_function, absolute_import,
+                        unicode_literals)
+
+import os
+import sys
+
+try:
+    from setuptools import setup
+    setup
+except ImportError:
+    from distutils.core import setup
+    setup
+
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist upload")
+    sys.exit()
+
+setup(
+    name="kplr",
+    version="0.0.1",
+    author="Daniel Foreman-Mackey",
+    author_email="danfm@nyu.edu",
+    packages=["kplr"],
+    url="https://github.com/dfm/kplr",
+    license="MIT",
+    description="Tools for working with Kepler data in Python",
+    long_description=open("README.rst").read(),
+    classifiers=[
+        # "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ],
+)
