3a040c69938f9d00c3960f4657f7ef42b8aeb924
Dan F-M <danfm@nyu.edu>
2013-04-23 13:56:49 -0400

    adding data handling, untrending and some constants

--- kplr/__init__.py
+++ kplr/__init__.py
@@ -2,4 +2,5 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import absolute_import
+from .kplr import TIME_BASELINE, EXPOSURE_TIMES
 from .mast import API
---
+++ kplr/kplr.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import (division, print_function, absolute_import,
+                        unicode_literals)
+
+__all__ = ["TIME_BASELINE", "EXPOSURE_TIMES"]
+
+# The zero point of the Kepler time basis (KBJD).
+TIME_BASELINE = 2454833.0
+
+# The (approximate) length of a Kepler exposure in seconds for short and long
+# cadence measurements.
+EXPOSURE_TIMES = [54.2, 1626.0]
