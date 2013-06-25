2ac53233fa30361499551fdfe75cad757a0e41da
Dan F-M <danfm@nyu.edu>
2013-06-25 15:35:07 -0400

    fixing stupid bug

--- kplr/__init__.py
+++ kplr/__init__.py
@@ -3,6 +3,5 @@
 
 from __future__ import absolute_import
 from .kplr import TIME_BASELINE, EXPOSURE_TIMES
-from .data import Dataset
 from .api import API, KOI, Planet, Star, LightCurve, TargetPixelFile
 from . import ld
