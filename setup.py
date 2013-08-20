a501eb0f7751355164f631d8675fb9f9eb181024
Dan F-M <danfm@nyu.edu>
2013-08-20 11:40:12 -0400

    madly bumping version numbers

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ if sys.argv[-1] == "publish":
 
 setup(
     name="kplr",
-    version="0.1.7",
+    version="0.1.8",
     author="Daniel Foreman-Mackey",
     author_email="danfm@nyu.edu",
     packages=["kplr"],
