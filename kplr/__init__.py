d3337ce5785d191f50bb0fed1e48644463273180
Dan F-M <danfm@nyu.edu>
2014-12-17 19:17:04 -0500

    adding Huber et al. (2014) + KIC join

--- kplr/__init__.py
+++ kplr/__init__.py
@@ -11,8 +11,8 @@ except NameError:
 
 if not __KPLR_SETUP__:
     __all__ = ["KBJD_ZERO", "EXPOSURE_TIMES", "API", "KOI", "Planet", "Star",
-               "LightCurve", "TargetPixelFile", "ld"]
+               "LightCurve", "TargetPixelFile", "ld", "huber"]
 
     from .kplr import KBJD_ZERO, EXPOSURE_TIMES
     from .api import API, KOI, Planet, Star, LightCurve, TargetPixelFile
-    from . import ld
+    from . import ld, huber
